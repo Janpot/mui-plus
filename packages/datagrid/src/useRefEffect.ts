47e11152980706f7b6c040f370999fe2f9174128
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-04-03 18:44:54 +0200

    cleanup implementation

--- packages/datagrid/src/useRefEffect.ts
+++ packages/datagrid/src/useRefEffect.ts
@@ -4,34 +4,23 @@ export default function useRefEffect<T>(
   effect: (elm: T) => void | (() => void)
 ): React.RefCallback<T> {
   const cleanupRef = React.useRef<null | (() => void)>(null);
-  const prevElm = React.useRef<T>();
+  const effectRef = React.useRef(effect);
+  effectRef.current = effect;
 
-  const ref = React.useCallback(
-    (elm: T) => {
-      if (prevElm.current === elm) {
-        return;
+  const ref = React.useCallback((elm: T) => {
+    cleanupRef.current?.();
+    if (elm) {
+      const effectCleanup = effectRef.current(elm);
+      if (effectCleanup) {
+        cleanupRef.current = () => {
+          effectCleanup();
+          cleanupRef.current = null;
+        };
       }
-      prevElm.current = elm;
-      if (cleanupRef.current) {
-        cleanupRef.current();
-        cleanupRef.current = null;
-      }
-      if (elm) {
-        cleanupRef.current = effect(elm) || null;
-      }
-    },
-    [effect]
-  );
+    }
+  }, []);
 
-  React.useEffect(
-    () => () => {
-      if (cleanupRef.current) {
-        cleanupRef.current();
-        cleanupRef.current = null;
-      }
-    },
-    []
-  );
+  React.useEffect(() => () => cleanupRef.current?.(), []);
 
   return ref;
 }
