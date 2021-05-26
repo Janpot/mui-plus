a8afef53dcb5c1dff413674b9da2c3143ab9785c
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-05-26 23:42:25 +0200

    clean up ssr

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -5,12 +5,6 @@ import { AppProps } from 'next/app';
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    jssStyles?.parentElement?.removeChild(jssStyles);
-  }, []);
-
   return (
     <React.Fragment>
       <Head>
